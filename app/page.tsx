ace3fc44fe245613008fb950fe4067d97d8c12d3
Bayu Aslama Zulfikar Ahmad <bayuaslamaa@gmail.com>
2025-02-22 16:50:03 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import Head from 'next/head';
 
 export default function LandingPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <Head>
+        <title>Waiting List Mindfuls.id</title>
+      </Head>
       <header className="mb-8">
-        <h1 className="text-3xl font-bold text-center text-blue-500">Welcome to Mindful Waiting List</h1>
-        <p className="text-center text-gray-600">Join our waiting list and stay updated!</p>
+        <h1 className="text-3xl font-bold text-center text-blue-500">Selamat Datang di Daftar Tunggu Mindfuls</h1>
+        <p className="text-center text-gray-600">Bergabunglah dengan daftar waiting list kami dan dapatkan update terbaru!</p>
       </header>
 
       <main className="flex flex-col items-center gap-4">
@@ -14,10 +18,18 @@ export default function LandingPage() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Join Waiting List
+          Bergabung dengan Daftar Tunggu
         </a> */}
         <a
-          className="w-full max-w-xs py-3 px-6 bg-green-500 text-white rounded-lg text-center hover:bg-green-600 transition"
+          className="w-full max-w-xs py-3 px-6 bg-white text-black rounded-lg text-center hover:bg-green-600 transition"
+          href="https://chat.whatsapp.com/FN3MWP1ATDwGR48UeQE6bv"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Daftar Waiting List
+        </a>
+        <a
+          className="w-full max-w-xs py-3 px-6 bg-white text-black rounded-lg text-center hover:bg-green-600 transition"
           href="https://calendly.com/arbaincare"
           target="_blank"
           rel="noopener noreferrer"
@@ -27,8 +39,14 @@ export default function LandingPage() {
       </main>
 
       <footer className="mt-8 text-center text-gray-500">
-        <p>© 2025 Mindful by Arbain. All rights reserved.</p>
+        <p>© 2025 Mindful oleh Arbain. Semua hak dilindungi.</p>
+        <p>Mindfuls.id adalah platform super app psikologi dan konseling muslim pertama di Indonesia.</p>
       </footer>
     </div>
   );
 }
+
+export const metadata = {
+  title: 'Waiting List Mindfuls.id',
+  description: 'Some description here...',
+};
