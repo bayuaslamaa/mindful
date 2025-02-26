3ba5f25e8f294db377aa4ffe6f50a88c4926881f
Bayu Aslama Zulfikar Ahmad <bayuaslamaa@gmail.com>
2025-02-26 16:04:05 +0700

    update landing page to donasi

---
+++ app/modal.tsx
@@ -0,0 +1,56 @@
+'use client'
+import { useState } from 'react';
+
+const Modal: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+            <button
+                type="button"
+                onClick={() => setIsOpen(true)}
+                className="bg-blue-500 text-white py-2 px-4 rounded"
+            >
+                Jadwalkan Konsultasi Gratis
+            </button>
+
+            {isOpen && (
+                <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+                    <div className="bg-white p-4 rounded-lg shadow-lg">
+                        <h3 className="text-xl font-bold text-center text-black">Pilih Opsi Konsultasi</h3>
+                        <p className="text-center mt-2 text-black">
+                            Kesehatan mental adalah bagian penting dari kesejahteraan kita. Dengan berdonasi, Anda membantu menyediakan akses ke layanan kesehatan mental yang sangat dibutuhkan. Mari bersama-sama menciptakan Indonesia yang lebih peduli dan mendukung kesehatan mental.
+                        </p>
+                        <div className="flex flex-col gap-4 mt-4">
+                            <a
+                                className="w-full max-w-xs py-3 px-6 bg-yellow-500 text-white rounded-lg text-center hover:bg-yellow-600 transition"
+                                href="https://lynk.id/mindfuls"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Donasi Dulu
+                            </a>
+                            <a
+                                className="w-full max-w-xs py-3 px-6 bg-yellow-300 text-black rounded-lg text-center hover:bg-green-600 transition"
+                                href="https://calendly.com/arbaincare"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Belum Sekarang dan Lanjut ke Konsultasi
+                            </a>
+                            <button
+                                type="button"
+                                onClick={() => setIsOpen(false)}
+                                className="text-gray-600 hover:text-gray-800 mt-2"
+                            >
+                                Tutup
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default Modal;
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+
+import React from 'react';
 import Head from 'next/head';
+import Modal from './modal'; // Import the Modal component
+
+export const metadata = {
+  title: 'Waiting List Mindfuls.id',
+  description: 'Some description here...',
+};
 
 export default function LandingPage() {
   return (
@@ -7,19 +15,11 @@ export default function LandingPage() {
         <title>Waiting List Mindfuls.id</title>
       </Head>
       <header className="mb-8">
-        <h1 className="text-3xl font-bold text-center text-blue-500">Selamat Datang di Daftar Tunggu Mindfuls</h1>
-        <p className="text-center text-gray-600">Bergabunglah dengan daftar waiting list kami dan dapatkan update terbaru!</p>
+        <h1 className="text-3xl font-bold text-center text-blue-500">Selamat Datang di Mindfuls</h1>
+        <p className="text-center text-gray-600">Bergabunglah dengan daftar waiting list kami atau coba konsultasi gratis!</p>
       </header>
 
       <main className="flex flex-col items-center gap-4">
-        {/* <a
-          className="w-full max-w-xs py-3 px-6 bg-blue-500 text-white rounded-lg text-center hover:bg-blue-600 transition"
-          href="https://example.com/waiting-list"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Bergabung dengan Daftar Tunggu
-        </a> */}
         <a
           className="w-full max-w-xs py-3 px-6 bg-white text-black rounded-lg text-center hover:bg-green-600 transition"
           href="https://chat.whatsapp.com/FN3MWP1ATDwGR48UeQE6bv"
@@ -28,13 +28,20 @@ export default function LandingPage() {
         >
           Daftar Waiting List
         </a>
+
+        {/* Button to open modal */}
+        <Modal />
+
+        <div className="text-center mb-2 mt-4">
+          <p className="text-xl text-gray-600">Mari berkontribusi untuk kesehatan mental Indonesia yang lebih baik</p>
+        </div>
         <a
-          className="w-full max-w-xs py-3 px-6 bg-white text-black rounded-lg text-center hover:bg-green-600 transition"
-          href="https://calendly.com/arbaincare"
+          className="w-full max-w-xs py-3 px-6 bg-yellow-500 text-white rounded-lg text-center hover:bg-yellow-600 transition"
+          href="https://lynk.id/mindfuls"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Konsul Gratis
+          Donasi Peduli Kesehatan Mental
         </a>
       </main>
 
@@ -46,7 +53,3 @@ export default function LandingPage() {
   );
 }
 
-export const metadata = {
-  title: 'Waiting List Mindfuls.id',
-  description: 'Some description here...',
-};
